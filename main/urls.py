e6ea33478cc2cd1343cd47bd5bb75e0a567e8e3b
Ogier Bouvier <ogier.bouvier@epfl.ch>
2015-08-31 18:00:30 +0200

    Url setup for contact form

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
-from .views import MainPageView, NewsDetailView
+from .views import MainPageView, NewsDetailView, ContactFormHandleView
 
 urlpatterns = [
     url(r'^$', MainPageView.as_view(), name='mainpage'),
     url(r'^(?P<pk>\d+)$', NewsDetailView.as_view(), name='news-detail'),
+    url(r'^contact$', ContactFormHandleView.as_view(), name='contact-handle'),
 ]
