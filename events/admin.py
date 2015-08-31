06b8b1593476f91163e0a37c2fac9a87d749141c
Ogier Bouvier <ogier.bouvier@epfl.ch>
2015-08-31 17:59:13 +0200

    Fixed admin interface for events

--- events/admin.py
+++ events/admin.py
@@ -4,8 +4,8 @@ from .models import Event
 
 
 class EventAdmin(admin.ModelAdmin):
-    fields = ['name', 'owner', 'description', 'datetime']
-
+    fields = ['name', 'owner', 'description', 'datetime', 'max_players',
+              'gallery', 'duration']
     list_display = ('name', 'owner', 'datetime')
 
 admin.site.register(Event, EventAdmin)
