c3088684cf7c30963ad3da02362c9e56679b9451
Ogier Bouvier <ogier.bouvier@epfl.ch>
2015-08-31 17:59:39 +0200

    Improvements in event model

--- events/models.py
+++ events/models.py
@@ -1,5 +1,7 @@
 # coding: utf-8
 
+from datetime import timedelta
+
 from django.db import models
 from django.core.urlresolvers import reverse
 from django.contrib.auth.models import User
@@ -13,13 +15,17 @@ class Event(models.Model):
     """
     Evenement générique (week-end, murder, soirées libres, ...)
     """
-    name = models.CharField(max_length=100, default=_('Nouvel événement'))
-    owner = models.ForeignKey(User)
-    place = models.CharField(max_length=200)
-    datetime = models.DateTimeField(default=timezone.now)
-    description = models.TextField()
-    max_players = models.IntegerField()
-    gallery = models.ForeignKey(Gallery, default=None, related_name='event')
+    name = models.CharField(max_length=100, default=_('Nouvel événement'),
+                            verbose_name=_("Nom"))
+    owner = models.ForeignKey(User, verbose_name=_("Organisateur"))
+    place = models.CharField(max_length=200, verbose_name=_("Lieu"))
+    datetime = models.DateTimeField(default=timezone.now,
+                                    verbose_name=_("Date et heure"))
+    duration = models.DurationField(verbose_name=_("Durée"))
+    description = models.TextField(verbose_name=_("Description"))
+    max_players = models.IntegerField(verbose_name=_("Nombre max de joueurs"))
+    gallery = models.ForeignKey(Gallery, default=None, related_name='event',
+                                verbose_name=_("Gallerie photo"))
 
     def get_absolute_url(self):
         return reverse('event-detail', kwargs={'pk': self.pk})
