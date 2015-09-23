10ba36504ee1652c821b27c5d6e18ec221148904
Ogier Bouvier <ogier.bouvier@epfl.ch>
2015-09-23 11:46:32 +0200

    A few views related to campaign management

--- events/views.py
+++ events/views.py
@@ -4,18 +4,92 @@ from django.conf import settings
 from django.core.urlresolvers import reverse_lazy
 from django.core.mail import send_mail
 from django.shortcuts import HttpResponseRedirect
-from django.forms import Form, CharField, Textarea, TextInput
-from django.views.generic import DetailView, ListView, UpdateView, FormView
+from django.forms import Form, ModelForm, CharField, Textarea, TextInput
+from django.views.generic import (DetailView, ListView, UpdateView, FormView,
+                                  CreateView)
 from django.utils import timezone
 from django.utils.translation import ugettext_lazy as _
 
-from .models import Event, Edition
+from .models import Event, Edition, Campaign
 from members.views import LoginRequiredMixin
 
 
 def redirect_to_edition(pk):
     return HttpResponseRedirect(reverse_lazy('edition-detail',
                                              kwargs={'pk': pk}))
+
+
+def redirect_to_campaign(pk):
+    return HttpResponseRedirect(reverse_lazy('campaign-detail',
+                                             kwargs={'pk': pk}))
+
+
+class CampaignCreateForm(ModelForm):
+    def is_valid(self):
+        if not super(CampaignCreateForm, self).is_valid():
+            return False
+        out = True
+        if not self.start > timezone.now():
+            self._errors.append('Votre campagne ne peut pas démarrer dans le passé !')
+            out = False
+
+        if not self.max_players > 0:
+            self._errors.append('Votre campagne ne peut pas avoir un nombre négatif de joueurs !')
+            out = False
+        return out
+
+    class Meta:
+        model = Campaign
+        fields = ['name', 'description', 'max_players', 'start', ]
+
+
+class CampaignPropositionView(CreateView, LoginRequiredMixin):
+    model = Campaign
+    template_name = 'events/new_campaign.html'
+    form_class = CampaignCreateForm
+
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super(CampaignPropositionView, self).form_valid(form)
+
+
+class CampaignDetailView(DetailView, LoginRequiredMixin):
+    model = Campaign
+    template_name = 'events/campaign_detail.html'
+    context_object_name = 'campaign'
+
+
+class CampaignListView(ListView, LoginRequiredMixin):
+    model = Campaign
+    template_name = 'events/campaign_list.html'
+    context_object_name = 'campaign_list'
+
+    def get_queryset(self):
+        return Campaign.objects.filter(running=True).filter(open_for_registration=True).order_by('-start')
+
+
+class CampaignRegisterView(UpdateView, LoginRequiredMixin):
+    model = Campaign
+    template_name = 'events/register_campaign.html'
+
+    def post(self, request, *args, **kwargs):
+        self.get_object().register_user(request.user)
+        return super(CampaignRegisterView, self).post(request, *args, **kwargs)
+
+    def get_context_data(self):
+        context = super(UpdateView, self).get_context_data()
+        object = self.get_object()
+        context['registered'] = self.request.user in object.participants.all()
+        return context
+
+
+class CampaignUnregisterView(UpdateView, LoginRequiredMixin):
+    model = Campaign
+
+    def get(self, request, *args, **kwargs):
+        obj = self.get_object()
+        obj.unregister_user(request.user)
+        return redirect_to_campaign(obj.pk)
 
 
 class EventPropositionForm(Form):
@@ -59,20 +133,16 @@ class RegisterEditionView(UpdateView, LoginRequiredMixin):
     model = Edition
 
     def get(self, request, *args, **kwargs):
-        obj = self.get_object()
-        obj.participants.add(request.user)
-        obj.save()
-        return redirect_to_edition(obj.pk)
+        self.get_object().register_user(request.user)
+        return redirect_to_edition(self.get_object().pk)
 
 
 class UnregisterEditionView(UpdateView, LoginRequiredMixin):
     model = Edition
 
     def get(self, request, *args, **kwargs):
-        obj = self.get_object()
-        obj.participants.remove(request.user)
-        obj.save()
-        return redirect_to_edition(obj.pk)
+        self.get_object().unregister_user(request.user)
+        return redirect_to_edition(self.get_object().pk)
 
 
 class EventListView(ListView):
