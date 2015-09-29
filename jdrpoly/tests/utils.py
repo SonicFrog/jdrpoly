bdb25b2f760a4d7738bc738c2a2318f0d6a651be
Ogier Bouvier <ogier.bouvier@epfl.ch>
2015-09-29 10:16:52 +0200

    Slight changes in test utils

--- jdrpoly/tests/utils.py
+++ jdrpoly/tests/utils.py
@@ -37,10 +37,15 @@ class AuthenticatedTestCase(TestCase):
         return request
 
     def makeMemberUser(self):
+        user = self.makeUser()
+        user.profile.until = (timezone.now() + datetime.timedelta(1)).date()
+        user.profile.save()
+        return user
+
+    def makeUser(self):
         user = User(username=randomword(20))
         user.set_password(self.PASSWORD)
         user.save()
-        user.profile.until = (timezone.now() + datetime.timedelta(1)).date()
         self.users.append(user)
         return user
 
@@ -58,6 +63,10 @@ def randomchr():
 
 def randomword(length):
     return ''.join([randomchr() for i in range(0, length)])
+
+
+def randomsentence(wordcount):
+    return ' '.join([randomword(10) for i in range(0, wordcount)])
 
 
 def setup_view(view, request, *args, **kwargs):
