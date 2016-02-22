8228a0077009ac05211e11c8587f68901110f4cf
Ogier Bouvier <ogier.bouvier@epfl.ch>
2016-02-22 18:41:29 +0100

    Updated SvZ app with views implementation

--- svz/models.py
+++ svz/models.py
@@ -2,4 +2,58 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Player(models.Model):
+    sciper = models.IntegerField(default=0, unique=True)
+    name = models.TextField(max_length=200)
+    contaminations = models.IntegerField(default=0)
+    token_spent = models.IntegerField(default=0)
+    zombie = models.BooleanField(default=False)
+    revive_count = models.IntegerField(default=0)
+
+    @classmethod
+    def generate_sciper():
+        exists = Player.objects().filter(sciper__lt=10000).count() < 0
+        new = None
+        if exists:
+            greatest = Player.objects().filter(sciper__lt=10000)[0]
+            new = greatest + 1
+        else:
+            new = 0
+        return new
+
+    @classmethod
+    def create(name, sciper=None):
+        if sciper is None:
+            sciper = Player.generate_sciper()
+        player = Player(sciper=sciper)
+        player.save()
+
+    def has_contaminated(self, other):
+        if self.zombie:
+            if not other.contaminate():
+                return False
+            self.contamination += 1
+            self.save()
+        return self.zombie
+
+    def contaminate(self):
+        if not self.zombie:
+            self.zombie = True
+            self.save()
+        return not self.zombie
+
+    def revive(self):
+        if self.zombie:
+            self.zombie = False
+            self.revive_count += 1
+            self.save()
+        return self.zombie
+
+    def spend_token(self, count):
+        self.token_spent += count
+        self.save()
+        return self.token_spent
+
+    class Meta:
+        ordering = ('sciper', 'name')
--- svz/views.py
+++ svz/views.py
@@ -1,3 +1,129 @@
-from django.shortcuts import render
+# coding: utf-8
 
-# Create your views here.
+from django.db.models import Q
+from django.http import JsonResponse
+from django.views.generic.detail import BaseDetailView
+from django.views.generic import (UpdateView, TemplateView,
+                                  ListView)
+
+from members.views import StaffRequiredMixin, LoginRequiredMixin
+from .models import Player
+
+
+def get_player(pk):
+    return Player.objects().get(pk=pk)
+
+
+def update_context(view, key, value):
+    context = view.get_context_data()
+    context[key] = value
+    return context
+
+
+class JsonResponseMixin(object):
+    def render_to_Json_response(self, context, **response_kwargs):
+        return JsonResponse(
+            self.get_data(context),
+            **response_kwargs
+        )
+
+    def get_data(self, context):
+        return context
+
+
+class JsonView(JsonResponseMixin, TemplateView):
+    def render_to_error(self, message, **kwargs):
+        context = update_context(self, 'message', message)
+        context['error'] = True
+        return self.render_to_Json_response(context, **kwargs)
+
+    def render_to_response(self, context, **response_kwargs):
+        return self.render_to_Json_response(context, **response_kwargs)
+
+
+class JsonDetailView(JsonResponseMixin, BaseDetailView):
+    def render_to_response(self, context, **kwargs):
+        return self.render_to_Json_response(context, **kwargs)
+
+
+class AdminView(LoginRequiredMixin, TemplateView):
+    """
+    Main view displaying admin panel
+    """
+    template_name = 'svz/admin.html'
+
+
+class InfoView(TemplateView):
+    template_name = 'svz/main.html'
+
+
+class ContaminateView(StaffRequiredMixin, JsonView):
+    """
+    View used to contaminate a player and return Json values
+    """
+    def post(self, request, *args, **kwargs):
+        pk1 = request.POST.get('player')
+        pk2 = request.POST.get('target')
+        contaminator = get_player(pk1)
+        contaminated = get_player(pk2)
+        context = self.get_context_data()
+
+        if contaminator.has_contaminated(contaminated):
+            context['message'] = "Contamination réussie !"
+        else:
+            return self.render_to_error("Contamination impossible !", **kwargs)
+        return self.render_to_response(context, **kwargs)
+
+
+class ReviveView(StaffRequiredMixin, JsonView):
+    """
+    View used to revive a player and return Json
+    """
+    def post(self, request, *args, **kwargs):
+        message = "Joueur soigné !"
+        pk = request.POST.get('pk')
+        player = get_player(pk)
+
+        if not player.revive():
+            return self.render_to_error("Ce joueur n'est pas un zombie !",
+                                        **kwargs)
+
+        context = update_context(self, 'message', message)
+        return self.render_to_response(context, **kwargs)
+
+
+class TokenView(StaffRequiredMixin, JsonResponseMixin, UpdateView):
+    """
+    View used with AJAX to spend token for a player
+    """
+    model = Player
+
+    def post(self, request, *args, **kwargs):
+        player = self.get_object()
+        count = self.request.POST.get('count')
+        new_count = player.spend_token(count)
+        context = update_context(self, 'token', new_count)
+        return self.render_to_response(context, **kwargs)
+
+
+class RegisterPlayerView(StaffRequiredMixin, JsonView):
+    def post(self, request, *args, **kwargs):
+        name = request.POST.get('name')
+        sciper = request.POST.get('sciper')
+        Player.create(name, sciper)
+        context = update_context(self, 'message', "Joueur créé !")
+        return self.render_to_response(context, **kwargs)
+
+
+class JsonPlayerView(StaffRequiredMixin, JsonDetailView):
+    model = Player
+
+
+class JsonPlayerFindView(StaffRequiredMixin, JsonResponseMixin, ListView):
+    model = Player
+
+    def get_queryset(self):
+        name = self.kwargs['name']
+        sciper = self.kwargs['sciper']
+        restrict = Q(name__startswith=name) | Q(sciper=sciper)
+        return Player.objects().filter(restrict)
