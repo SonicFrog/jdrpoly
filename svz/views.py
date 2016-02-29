2ed97c451eeb61985bc7fac1a2448f8f70c221fb
Ogier Bouvier <ogier.bouvier@epfl.ch>
2016-02-29 18:50:44 +0100

    Working player find view

--- svz/views.py
+++ svz/views.py
@@ -2,27 +2,43 @@
 
 from django.views.generic import TemplateView
 from django.utils.translation import ugettext_lazy as _
+from django.shortcuts import get_object_or_404
 
 from members.views import StaffRequiredMixin, LoginRequiredMixin
 from .models import Player
 
 from rest_framework import serializers, viewsets, views
+from rest_framework.generics import UpdateAPIView, RetrieveAPIView
 
 
 def get_player(pk):
     return Player.objects().get(pk=pk)
 
 
-def update_context(view, key, value):
-    context = view.get_context_data()
-    context[key] = value
-    return context
+class POSTMultipleFieldLookupMixin(object):
+    def get_object(self):
+        queryset = self.get_queryset()
+        queryset = self.filter_queryset(queryset)
+        filter = {}
+        for field in self.lookup_fields:
+            filter[field] = self.request.POST.get(field)
+        return get_object_or_404(queryset, **filter)
+
+
+class MultipleFieldLookupMixin(object):
+    def get_object(self):
+        queryset = self.get_queryset()
+        queryset = self.filter_queryset(queryset)
+        filter = {}
+        for field in self.lookup_fields:
+            filter[field] = self.kwargs[field]
+        return get_object_or_404(queryset, **filter)
 
 
 class PlayerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Player
-        fields = ('sciper', 'name', 'contaminations', 'zombie',)
+        fields = ('sciper', 'name', 'contaminations', 'zombie', 'token_spent')
 
 
 class PlayerViewSet(viewsets.ModelViewSet):
@@ -50,50 +66,30 @@ class ContaminateView(StaffRequiredMixin, views.APIView):
         pk2 = request.POST.get('target')
         contaminator = get_player(pk1)
         contaminated = get_player(pk2)
-        context = self.get_context_data()
 
-        if contaminator.has_contaminated(contaminated):
-            context['message'] = _("Contamination réussie !")
-        else:
-            return self.render_to_error(_("Contamination impossible !"),
-                                        **kwargs)
-
-        return self.render_to_response(context, **kwargs)
+        if not contaminator.has_contaminated(contaminated):
+            raise ValueError("")
+        return super(ContaminateView, self).post(request, *args, **kwargs)
 
 
-class ReviveView(StaffRequiredMixin, views.APIView):
-    """
-    View used to revive a player and return Json
-    """
-    def post(self, request, *args, **kwargs):
-        message = "Joueur soigné !"
-        pk = request.POST.get('pk')
-        player = get_player(pk)
-
-        if not player.revive():
-            return self.render_to_error(_("Ce joueur n'est pas un zombie !"),
-                                        **kwargs)
-
-        context = update_context(self, 'message', message)
-        return self.render_to_response(context, **kwargs)
-
-
-class TokenView(StaffRequiredMixin, views.APIView):
+class TokenView(StaffRequiredMixin, UpdateAPIView):
     """
     View used with AJAX to spend token for a player
     """
     model = Player
 
-    def post(self, request, *args, **kwargs):
-        player = self.get_object()
-        count = self.request.POST.get('count')
-        new_count = player.spend_token(count)
-        context = update_context(self, 'token', new_count)
-        return self.render_to_response(context, **kwargs)
+    def perform_update(self, serializer):
+        old_player = self.get_object()
+        new_player = serializer.save()
+        old_player.spend_token(new_player.token)
+        old_player.save()
 
 
-class PlayerFindView(views.APIView):
+class PlayerFindView(MultipleFieldLookupMixin, RetrieveAPIView):
+    model = Player
+    lookup_fields = ('sciper', )
+
+    serializer_class = PlayerSerializer
 
     def get_queryset(self):
-        name = self.kwargs['name']
-        return Player.objects().filter(name__startswith=name)
+        return Player.objects.all()
