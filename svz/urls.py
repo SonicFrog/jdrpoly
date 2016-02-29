349c4fd8e3d5b1d3a0ee8860768bc9882d4a753f
Ogier Bouvier <ogier.bouvier@epfl.ch>
2016-02-29 15:48:19 +0100

    Url mappings for the SvZ module

---
+++ svz/urls.py
@@ -0,0 +1,21 @@
+from django.conf.urls import url
+
+from .views import (InfoView, AdminView, ContaminateView,
+                    ReviveView, TokenView, PlayerFindView, PlayerViewSet)
+
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'json/players/', PlayerViewSet)
+
+urlpatterns = [
+    url(r'^$', InfoView.as_view(), name='svz-main'),
+    url(r'^admin$', AdminView.as_view(), name='svz-admin'),
+
+    url(r'^json/contaminate$', ContaminateView.as_view(),
+        name='svz-contaminate'),
+    url(r'^json/revive$', ReviveView.as_view(), name='svz-revive'),
+    url(r'^json/token$', TokenView.as_view(), name='svz-token'),
+    url(r'^json/find/(?P<name>\w+)$',
+        PlayerFindView.as_view(), name='svz-find'),
+] + router.urls
