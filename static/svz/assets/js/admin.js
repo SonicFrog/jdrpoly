5390ef247cfccb3b65c138b146b06be29107deae
Ogier Bouvier <ogier.bouvier@epfl.ch>
2016-03-24 12:47:23 +0100

    URL fixing and plugged the display player functionnality

--- static/svz/assets/js/admin.js
+++ static/svz/assets/js/admin.js
@@ -2,15 +2,20 @@ $( document ).ready(function() {
     $( "#search-form" ).submit(function( event ) {
         var name = $("#search-form-name").val();
         var sciper = $("#search-form-sciper").val();
-        $.get("/svz/json/find/"+sciper,
-               function (data) {
-                   alert("" + data.name);
-               })
-            .fail( function () {
-                alert("Impossible de trouver le joueur " +
-                      name + " " + sciper + ".");
-            });
+
+        fetch_player(sciper, update_form);
+
         event.preventDefault();
+
+        // $.get("/svz/json/find/"+sciper,
+        //        function (data) {
+        //            alert("" + data.name);
+        //        })
+        //     .fail( function () {
+        //         alert("Impossible de trouver le joueur " +
+        //               name + " " + sciper + ".");
+        //     });
+
     });
 
     $( "#update-form" ).submit(function( event ) {
@@ -20,15 +25,25 @@ $( document ).ready(function() {
 });
 
 /**
- * Updates a player
+ * Fills the given form with the given player info
+ **/
+function update_form(player) {
+    $("#demo-name").val(player.name);
+    $("#demo-sciper").val(player.sciper);
+    $("#token-spent").html(player.token_spent);
+    $("#contaminations").html(player.contaminations);
+    $("#zombie-status").prop('checked', player.zombie);
+}
+
+/**
+ * Updates a player (*this function expects the new total for each field*)
  * @param sciper Sciper number for this player
  * @param token The number of token spent
  * @param contaminate a boolean indicating zombie status
  **/
 function update_player(sciper, token, zombie, contaminations) {
     //URL is hardcoded for now but will be generated using django templates
-    var url = "/svz/json/{0}/".format(sciper);
-    var method = "patch";
+    var url = "/svz/json/players/" +sciper + "/";
 
     $.ajax({
         url: url,
@@ -41,6 +56,12 @@ function update_player(sciper, token, zombie, contaminations) {
     });
 }
 
+function fetch_player(sciper, callback) {
+    var url = "/svz/json/players/" + sciper + "/";
+
+    $.get(url, update_form).fail("Une erreur s'est produite !");
+}
+
 /**
  * Finds player by either sciper or name (this can return multiple players)
  * @param sciper Sciper number (partial or complete)
@@ -50,7 +71,7 @@ function update_player(sciper, token, zombie, contaminations) {
  * @param error Callback for other error
  **/
 function find_player(sciper, name, callback, notfound, error) {
-    var url = "/svz/player/find/{0}/{1}/".format(sciper, name);
+    var url = "/svz/player/find/"+ sciper + "/" + name + "/";
 
     $.get({
         url: url,
@@ -70,7 +91,7 @@ function find_player(sciper, name, callback, notfound, error) {
  **/
 function create_player(sciper, name, error) {
     //TODO: Fix hardcoded urls
-    var url = "/svz/player/";
+    var url = "/svz/json/players/";
 
     $.ajax({
         url: url,
